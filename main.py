cc62acf039d3eff540e39b9f37c1e98c0a6148df
Luis Felippe Lima Moraes <49735785+FelippeYT@users.noreply.github.com>
2026-04-10 18:19:41 -0300

    Enable message content intent in bot setup
    Added message content intent to the bot.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix=".", intents=intents)
 
 FILE = "players.json"
